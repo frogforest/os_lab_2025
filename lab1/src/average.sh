f37c3768b084382f7e14b68352b8b0892ef57565
frogforest <frogs.admirer@gmail.com>
2025-09-27 07:50:20 +0000

    akk is done

---
+++ lab1/src/average.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+if [ $# -eq 0 ]; then
+    echo "Количество: 0"
+    echo "Среднее: 0"
+    exit 0
+fi
+
+sum=0
+count=0
+for num in "$@"; do
+    sum=$((sum + num))
+    count=$((count + 1))
+done
+
+average=$((sum / count))
+echo "Количество: $count"
+echo "Среднее: $average"

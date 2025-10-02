b411955cb875451c65f9198e248b81e4bb684ff3
frogforest <frogs.admirer@gmail.com>
2025-10-02 15:48:23 +0000

    otredactirovano

--- lab1/src/average.sh
+++ lab1/src/average.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+
 if [ $# -eq 0 ]; then
     echo "Количество: 0"
     echo "Среднее: 0"
@@ -7,11 +8,17 @@ fi
 
 sum=0
 count=0
+
 for num in "$@"; do
     sum=$((sum + num))
     count=$((count + 1))
 done
 
-average=$((sum / count))
+if [ $count -ne 0 ]; then
+    average=$(( (sum + count/2) / count ))  # Округление к ближайшему целому
+else
+    average=0
+fi
+
 echo "Количество: $count"
 echo "Среднее: $average"
